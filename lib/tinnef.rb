bff51fbfa4ffdb6520208495709d5d68eb862243
Georg Ledermann <ledermann@gmx.de>
2010-05-18 16:00:02 +0200

    Documentation added

--- lib/tinnef.rb
+++ lib/tinnef.rb
@@ -1,6 +1,28 @@
 require 'tinnef/dir_patch'
 
 class TNEF
+  # = convert
+  #
+  # Extract the content of a winmail.dat file and 
+  # call the given block for every single file in it
+  # 
+  # Parameters:
+  #   content => Content of the winmail.dat file
+  # 
+  #   Options hash with this keys:
+  #     :command 
+  #       Full path of the tnf binary to execute (defaults to 'tnf', so it has to be in the path)
+  #
+  # Example:
+  #   content = File.new('winmail.dat').read
+  #   TNEF.convert(content, :command => '/opt/local/bin/tnef') do |temp_file|
+  #     unpacked_content = temp_file.read
+  #     unpacked_filename = File.basename(temp_file.path)
+  # 
+  #     File.open("/some/path/#{unpacked_filename}", 'w') do |new_file| 
+  #       new_file.write(unpacked_content)
+  #     end
+  #   end
   def self.convert(content, options={}, &block)
     command = options[:command] || 'tnef'
     
